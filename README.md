99901c177e8419c28a21426bd1038c393d3c2c5c
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-18 19:30:41 -0400

    Edit boilerplate code in App.tsx

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 The fastest way to start a React project using Vite, TypeScript, ESLint & TailwindCSS.
 
-
-# PROJECT WILL BE HOSTED ON NPM AT 12:00AM AUGUST 19TH 2022.
-## I deleted a test and didn't realise I couldn't reuse the name for 24 hours.
-
 ## How To Run
 
 ```
--- src-files/App.tsx
+++ src-files/App.tsx
@@ -1,7 +1,8 @@
 const App = () => (
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div className="h-screen flex flex-col justify-center items-center">
+    <h1 className="font-bold text-xl">create-r1-app</h1>
+    <p className="text-xl">Begin by editing src/App.tsx</p>
+  </div>
 );
 
 export default App;
